5ba58e9bf0fcddace1f7b26bb1e2015ada5f15fd
Piyushjh1212 <piyushjhariya@gmail.com>
2026-02-14 14:05:58 +0530

    Courses backend COnnection done

--- Backend/Controllers/CoursesController.js
+++ Backend/Controllers/CoursesController.js
@@ -32,3 +32,12 @@ export const getCoursesProducts = async (req, res) => {
     }
 }
 
+
+export const getAllCourses = async (req, res) => {
+  try {
+    const courses = await CoursesProducts.find();
+    res.status(200).json(courses);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
