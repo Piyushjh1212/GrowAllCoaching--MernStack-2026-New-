3816affe0a3696e25cbabbd9fa2e1d4bce0febe9
Piyushjh1212 <piyushjhariya65@gmail.com>
2026-03-12 01:08:05 +0530

    autologuot and check user if exist go to userdasbooard else login page

--- Frontend/src/Route/PublicRoute.jsx
+++ Frontend/src/Route/PublicRoute.jsx
@@ -1,12 +1,51 @@
-import { Navigate } from "react-router-dom"
+import { Navigate } from "react-router-dom";
+import { useEffect, useState } from "react";
 
 export const PublicRoute = ({ children }) => {
 
-    const token = localStorage.getItem("token");
+  const [loading, setLoading] = useState(true);
+  const [isAuth, setIsAuth] = useState(false);
 
-    if (token) {
-        return <Navigate to="/" />;
-    }
+  const token = localStorage.getItem("token");
 
-    return children;
-}
+  useEffect(() => {
+    const checkAuth = async () => {
+
+      if (!token) {
+        setLoading(false);
+        return;
+      }
+
+      try {
+
+        const res = await fetch(
+          "http://localhost:5000/api/v1/UserLoginSignup/profile",
+          {
+            headers: {
+              Authorization: `Bearer ${token}`
+            }
+          }
+        );
+
+        if (res.ok) {
+          setIsAuth(true);
+        } else {
+          localStorage.removeItem("token");
+        }
+
+      } catch (err) {
+        console.error(err);
+        localStorage.removeItem("token");
+      }
+
+      setLoading(false);
+    };
+
+    checkAuth();
+
+  }, [token]);
+
+  if (loading) return <p>Checking...</p>;
+
+  return isAuth ? <Navigate to="/" /> : children;
+};
